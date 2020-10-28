bab6737728a948ab7078da390d2fbb7a2e75dd5c
Siddharth Bhardwaj <siddharth.bhardwaj2051999@gmail.com>
2020-10-28 16:57:04 +0530

    added process.env.port for deployment

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const urlShortener = require("node-url-shortener");
 
 const app = express();
 const path = require("path");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded());
